18bf4f1a1aabb48b59e72fc202504df00cc4d3b9
Sam O'Brien-Olinger <samtobo@yahoo.co.uk>
2026-09-02 15:44:19 -0700

    Rename site and add creator link

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "About | My New Life in Ireland",
+  title: "About | A New Life in Ireland",
   description:
-    "About the educational purpose, sources, creator, copyright and ownership of My New Life in Ireland.",
+    "About the educational purpose, sources, creator, copyright and ownership of A New Life in Ireland.",
 };
 
 export default function AboutLayout({
